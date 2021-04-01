6e9827cad16693eb8f420b093bf2cf32c1f8d986
Oleg-Akinfiev <77986926+Oleg-Akinfiev@users.noreply.github.com>
2021-04-01 23:14:33 +0300

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,49 @@
+$(function(){
+
+    /*Filter*/
+    let filter = $("[data-filter]");
+
+    filter.on("click", function(event) {
+        event.preventDefault();
+
+        let cat =$(this).data('filter');
+
+        if(cat == 'all') {
+            $("[data-cat]").removeClass('hide')
+        } else {
+            $("[data-cat]").each(function() {
+                let workCat = $(this).data('cat');
+
+                if(workCat != cat) {
+                    $(this).addClass('hide');
+                } else {
+                    $(this).removeClass('hide');
+                }
+            });
+        }
+    });
+
+    /*Modal*/
+    let modalCall = $("[data-modal]");
+    let modalClose = $("[data-close]");
+
+    modalCall.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalId = $this.data('modal');
+
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
+    });
+
+    modalClose.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalParent = $this.parents('.modal');
+
+        modalParent.removeClass('show');
+        $("body").removeClass('no-scroll');
+    });
+});
